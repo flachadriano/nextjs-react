c1082161a1f705c472f0c05c0da10b7bf69e8626
Adriano Flach de Araujo <adriano.spintec@oncologiador.corp>
2023-07-31 18:31:33 -0300

    another way to redirect

--- pages/clients/index.tsx
+++ pages/clients/index.tsx
@@ -1,7 +1,25 @@
+import { useRouter } from 'next/router'
+
 export default function Clients() {
+  const router = useRouter()
+  const clients = [
+    {id: 'flach', name: 'Adriano Flach'}
+  ]
+
+  function clientPageHandler(client: any) {
+    router.push({
+      pathname: '/clients/[id]',
+      query: { id: client.id }
+    })
+  }
+
   return (
     <div>
       <h1>Clients</h1>
+      {clients.map(client => (
+        <button key={client.id} onClick={() => clientPageHandler(client)}>{client.name}</button>
+      ))}
+      <button></button>
     </div>
   )
 }
