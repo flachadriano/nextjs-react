1dbc16ef1db12ae1e47b047f699bddc28dd256ac
Adriano Flach de Araujo <adriano.spintec@oncologiador.corp>
2023-07-31 18:24:56 -0300

    redirect through source code

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,22 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+  
+  function clientFolderHandler() {
+    console.log('send to another page through source code');
+    router.push('/clients')
+  }
+
   return (
     <div>
       <h1>My Application</h1>
       <ul>
         <li><Link href="/portfolio">Portfolio</Link></li>
-        <li><Link href="/clients">Clients</Link></li>
+        <li><button onClick={clientFolderHandler}>Clients</button></li>
       </ul>
       <hr></hr>
       <Component {...pageProps} />
