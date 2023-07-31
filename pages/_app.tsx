fdd1e56b54017c7ef3fe889b91a739c08538dce9
Adriano Flach de Araujo <adriano.spintec@oncologiador.corp>
2023-07-31 18:15:26 -0300

    add link

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,15 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Link from 'next/link'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div>
       <h1>My Application</h1>
+      <ul>
+        <li><Link href="/portfolio">Portfolio</Link></li>
+        <li><Link href="/clients">Clients</Link></li>
+      </ul>
       <hr></hr>
       <Component {...pageProps} />
     </div>
