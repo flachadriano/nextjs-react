c5b6ee4434808fdcca281bba2feeeeb166793d7c
Adriano Flach de Araujo <adriano.spintec@oncologiador.corp>
2023-08-01 18:12:13 -0300

    adding mock data

---
+++ 02-project/dummy-data.ts
@@ -0,0 +1,29 @@
+const DUMMY_EVENTS = [
+  {
+    id: 'e1',
+    title: 'Programming for everyone',
+    description: 'Everyone can learn to code! Yes, everyone',
+    location: 'Somestreet 25, 12345 San Somewhereo',
+    date: '2023-09-12',
+    image: 'images/code-everywhere.jpeg',
+    isFeatured: false,
+  },
+  {
+    id: 'e2',
+    title: 'Networking for introverts',
+    description: 'We Know: Networking is no fun if you are an introvert person.',
+    location: 'New Wall Street 5, 98765 New Work',
+    date: '2023-09-30',
+    image: 'images/introvert.jpg',
+    isFeatured: true
+  },
+  {
+    id: 'e3',
+    title: 'Network for extroverts',
+    description: 'You propably need no help with networking in general.',
+    localtion: 'My Street 12, 10115 Broke City',
+    date: '2023-09-28',
+    image: 'images/extrovert',
+    isFeatured: false
+  }
+]
