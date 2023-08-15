d1d3e0504bfc47bb1270c6e8bb633a39d7be6a8b
Adriano Flach de Araujo <adriano.spintec@oncologiador.corp>
2023-08-15 18:33:29 -0300

    add fallback to dynamic pages

--- 03-pre-render-and-data-fetch/pages/[pid].tsx
+++ 03-pre-render-and-data-fetch/pages/[pid].tsx
@@ -8,7 +8,7 @@ export async function getStaticProps({ params }: any) {
   const jsonData = await fs.readFile(filePath);
   const data = JSON.parse(jsonData.toString());
 
-  const product = data.products.find((product: {id: string}) => product.id = productId);
+  const product = data.products.find((product: {id: string}) => product.id == productId);
   return {
     props: {
       loadedProduct: product
@@ -20,10 +20,10 @@ export async function getStaticPaths() {
   return {
     paths: [
       { params: { pid: 'p1' } },
-      { params: { pid: 'p2' } },
       { params: { pid: 'p3' } }
     ],
-    fallback: false
+    fallback: true // it will generate a static page, when it is not in the paths options, e.g.: page of product 2
+    // fallback: 'blocking' // it will make the user wait, with loading of the browser, until the page is generated
   }
 }
 
@@ -35,6 +35,10 @@ interface ProductDetailPageProps {
 }
 
 export default function ProductDetailPage({ loadedProduct } : ProductDetailPageProps) {
+  if (!loadedProduct) {
+    return <p>Loading...</p>
+  }
+ 
   return (
     <>
       <h1>{loadedProduct.title}</h1>
