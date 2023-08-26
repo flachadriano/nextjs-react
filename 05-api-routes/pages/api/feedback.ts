792a82cb52c4e5513a7dde7f19dac57f253560fa
Adriano Flach de Araujo <adriano.spintec@oncologiador.corp>
2023-08-26 13:48:06 -0300

    api to load data

--- 05-api-routes/pages/api/feedback.ts
+++ 05-api-routes/pages/api/feedback.ts
@@ -2,10 +2,20 @@ import { readFileSync, writeFileSync } from 'fs';
 import type { NextApiRequest, NextApiResponse } from 'next'
 import path from 'path';
 
-
 type Data = {
   message: string
   feedback?: any
+  data?: any
+}
+
+function getPath() {
+  return path.join(process.cwd(), 'data', 'feedback.json');
+}
+
+function getData() {
+  const fileData = readFileSync(getPath());
+  const data = JSON.parse(fileData.toString());
+  return data;
 }
 
 export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
@@ -14,12 +24,12 @@ export default function handler(req: NextApiRequest, res: NextApiResponse<Data>)
     const feedback = req.body.feedback;
 
     const newFeedback = { id: new Date().toISOString(), email, feedback };
-    const filePath = path.join(process.cwd(), 'data', 'feedback.json');
-    const fileData = readFileSync(filePath);
-    const data = JSON.parse(fileData.toString());
+    const data = getData();
     data.push(newFeedback);
-    writeFileSync(filePath, JSON.stringify(data));
+    writeFileSync(getPath(), JSON.stringify(data));
     res.status(201).json({ message: 'Success', feedback: newFeedback });
+  } else if (req.method === 'GET') {
+    res.status(200).json({ message: 'Success', data: getData() })
   } else {
     res.status(200).json({ message: 'Blank response' })
   }
--- 05-api-routes/pages/index.tsx
+++ 05-api-routes/pages/index.tsx
@@ -1,6 +1,7 @@
-import { useRef } from "react"
+import { useEffect, useRef, useState } from "react"
 
 export default function Home() {
+  const [feedbacks, setFeedbacks] = useState<any[]>([]);
   const emailRef = useRef<HTMLInputElement>(null);
   const feedbackRef = useRef<HTMLInputElement>(null);
 
@@ -17,8 +18,14 @@ export default function Home() {
         'Content-Type': 'application/json'
       }
     }).then(res => res.json())
-      .then(data => console.log(data));
+      .then(data => setFeedbacks([...feedbacks, data.feedback]));
   }
+
+  const loadFeedbacks = () => fetch('/api/feedback').then(res => res.json()).then(json => setFeedbacks(json.data));
+
+  useEffect(() => {
+    loadFeedbacks();
+  }, []);
 
   return (
     <div>
@@ -34,6 +41,11 @@ export default function Home() {
         </div>
         <button type="submit">Submitt</button>
       </form>
+      <hr></hr>
+      <h1>Feedbacks</h1>
+      <div>
+        {feedbacks.map(feedback => <div key={feedback.email}>{feedback.feedback}</div>)}
+      </div>
     </div>
   )
 }
